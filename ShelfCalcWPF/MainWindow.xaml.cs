083d6f485d0256866139201ec8944c2cdcc4695a
Anton Alalaev <antonalalaev@hotmail.com>
2020-03-22 17:40:24 +0500

    Базовая версяи расчета старой полки

--- ShelfCalcWPF/MainWindow.xaml.cs
+++ ShelfCalcWPF/MainWindow.xaml.cs
@@ -24,5 +24,163 @@ namespace ShelfCalcWPF
         {
             InitializeComponent();
         }
+
+        private void Distance_TextInput(object sender, TextCompositionEventArgs e)
+        {
+           // nothing happen
+        }
+
+        private void Distance_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Пересчет
+            Recalculate();
+        }
+
+
+        private double RightRound(double Value, int Accuracy)
+        {
+            return Math.Round((Value * Math.Pow(10, Accuracy) + 0.499999), 0) / (Math.Pow(10, Accuracy));
+        }
+ 
+        private int normalShelfDistance(int Distance)
+        {
+
+            int retValue = 20;
+            while (retValue < Distance)
+            {
+                retValue = retValue + 25;
+            }
+            return retValue ;
+        }
+
+        private int normalStatH(int Distance)
+        {
+            int RetValue = 38;
+            if (Distance != 38)
+            {
+                if (Distance < 38)
+                {
+                    RetValue = 38;
+                }
+                else
+                {
+                    RetValue = normalShelfDistance(Distance + 1);
+                }
+            }
+            return RetValue;
+        }
+
+
+        private int normalShelfBottom(int Distance)
+        {
+            int RetValue = 38;
+            if (Distance != 38)
+            {
+                if (Distance < 38)
+                {
+                    RetValue = 38;
+                }
+                else
+                {
+                    if (Distance <= 63)
+                    {
+                        RetValue = 63;
+                    }
+                    else
+                    {
+                        RetValue = normalShelfDistance(Distance-18) + 18;
+                    }
+                    
+                }
+            }
+            return RetValue;
+        }
+
+        private int StoikaCalc(int distance, int Amount, int Visota)
+        {
+            int Step = 0;
+            int Height = 0;
+            int totalstep = 0;
+            for (int i = 1; i <= Amount+1; i++)
+            {
+                if (i == 1)
+                {
+                    //Первая полка
+                    Height = Visota;
+                    totalstep = (Height - 13) / 25;
+                }
+                else
+                {
+                    Step = 1;
+                    while (Step * 25 - 14 < distance) { Step++; }
+
+                    totalstep = totalstep + Step;
+                    Height = (totalstep * 25) - 3 + 16;
+                }
+            }
+            return Height-6;
+        }
+
+        private void Recalculate()
+        {
+            try
+            {
+                int ShelfDist = Convert.ToInt16(Distance.Text);
+                int ShelfAmount = Convert.ToInt16(Amount.Text);
+                int ShelfBottomDistance = Convert.ToInt16(Height.Text);
+                ShelfDist = normalShelfDistance(ShelfDist);
+                //int StatBottomDistance = Convert.ToInt16(statHeight.Text);
+
+                // расчетное расстояние между полками
+                CalculatedShelfDist.Text = ShelfDist.ToString();
+                // нормализуем ShelfbottomDistance
+                ShelfBottomDistance = normalShelfBottom(ShelfBottomDistance);
+
+                int StHeight = StoikaCalc(ShelfDist, ShelfAmount, ShelfBottomDistance);
+                StayHeight.Text = StHeight.ToString();
+                wFloor.Text = Convert.ToString(StHeight + 113);
+                FalseFloor.Text = Convert.ToString(StHeight + 133);
+                Welded.Text = Convert.ToString(StHeight + 183);
+                WorkShelf.Text = Convert.ToString(ShelfDist);
+                BotomCal.Text = Convert.ToString(ShelfBottomDistance);
+            }
+            catch(Exception ex)
+            {   }
+        }
+
+        private void Amount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Recalculate();
+        }
+
+        private void Height_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Recalculate();
+            CheckHeight();
+        }
+        private void CheckHeight()
+        {
+            try
+            {
+                if (Convert.ToInt16(Height.Text) < 38)
+                {
+                    Height.Background = new SolidColorBrush(Color.FromArgb(255, 222, 19, 19)); 
+                }
+                else
+                {
+                    Height.Background = new SolidColorBrush(Color.FromArgb(255, 195, 194, 194));
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+
+   
+
+        private void statHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Recalculate();
+
+        }
     }
 }
